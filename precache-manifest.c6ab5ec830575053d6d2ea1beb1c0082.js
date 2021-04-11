0ce0737d3530a115e22e5803402fa9173bc4cc40
Arun Singh <77932309+Arun9251@users.noreply.github.com>
2021-04-11 20:56:08 +0530

    Updates

---
+++ precache-manifest.c6ab5ec830575053d6d2ea1beb1c0082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601b9686cda102e657a9e347e881678e",
+    "url": "/Cinemeye-movie-app-clone/index.html"
+  },
+  {
+    "revision": "668bf72882ce74335c3a",
+    "url": "/Cinemeye-movie-app-clone/static/css/main.bd4c990e.chunk.css"
+  },
+  {
+    "revision": "2c612f22b7aa4b54e71c",
+    "url": "/Cinemeye-movie-app-clone/static/js/2.e3a914ce.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/Cinemeye-movie-app-clone/static/js/2.e3a914ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668bf72882ce74335c3a",
+    "url": "/Cinemeye-movie-app-clone/static/js/main.216d22cf.chunk.js"
+  },
+  {
+    "revision": "d9fc8b3bf691a0b43638",
+    "url": "/Cinemeye-movie-app-clone/static/js/runtime-main.afd7f646.js"
+  }
+]);
